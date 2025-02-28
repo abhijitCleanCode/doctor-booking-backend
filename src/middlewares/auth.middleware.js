b2a8cacc26d950e4f01defca937da40efe232516
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2025-02-28 17:44:03 +0530

    bearer issue fix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -61,7 +61,7 @@ export const VERIFY_FOR_USER = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", ""); // Fixing "Bearer" typo
+      req.header("Authorization")?.replace("Bearer ", "");
 
     console.log("auth middleware :: token : ", token);
 
